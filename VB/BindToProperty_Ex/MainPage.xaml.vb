7d1c109cca2cb2c75b82e703c55c4b4cedf71a9c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/BindToProperty_Ex/MainPage.xaml.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports System.Windows.Data
+Imports DevExpress.Xpf.Docking
+
+Namespace BindToProperty_Ex
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+
+			Dim targetControl As DependencyObject = dm1.GetItem("myLabel")
+			BindingOperations.SetBinding(targetControl, BaseLayoutItem.CaptionProperty, New Binding("Text") With {.Source = myTextEdit})
+		End Sub
+	End Class
+End Namespace
